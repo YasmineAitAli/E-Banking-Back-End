ce4d74522cc58359926264d55b5e1f0849ccaaab
AbdelhayAboulbaraket <abdelhay.aboulbaraket@gmail.com>
2021-05-19 20:13:29 +0100

    first commit

--- src/main/java/com/backend/EnsaBackApplication.java
+++ src/main/java/com/backend/EnsaBackApplication.java
@@ -9,8 +9,12 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 public class EnsaBackApplication {
 
 	public static void main(String[] args) {
-		
+		try {
 		SpringApplication.run(EnsaBackApplication.class, args);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
